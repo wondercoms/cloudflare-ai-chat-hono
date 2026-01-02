e0c2652a193bdaf4d2f68a5502043e0a0bd39719
H Y <hirohiro.yata@gmail.com>
2026-01-02 21:07:34 +0900

    bug fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,1 @@
-/**
- * HTMLエスケープ
- */
-export function escapeHtml(text: string): string {
-  return text
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;')
-}
+// ユーティリティ関数
